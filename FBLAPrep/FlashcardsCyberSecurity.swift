a91324b00fb0362c2e3c1ff6ed7ec4dc662f6252
Gonzalo Birrueta <silentcr7@gmail.com>
2019-02-27 11:44:14 -0800

    added next flashcard button and implemented progress bar with animation to flashcard view

--- FBLAPrep/FlashcardsCyberSecurity.swift
+++ FBLAPrep/FlashcardsCyberSecurity.swift
@@ -10,23 +10,61 @@ import Foundation
 import UIKit
 
 class FlashcardsCyberSecurity: UIViewController{
+    
     @IBOutlet weak var cardIMG: UIButton!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//      Styling for progress bar
+        progressView.layer.cornerRadius = 8
+        progressView.clipsToBounds = true
+        progressView.layer.sublayers![1].cornerRadius = 8
+        progressView.subviews[1].clipsToBounds = true
+    }
+    
     
     var isFlipped = false
+    var currentFlashcard = 0
+    let currentProgress = Progress(totalUnitCount: 10)
     
+//  all flashcard images in array
+    var frontImages = ["flashcardFrontDemo","flashcardFrontDemo2","flashcardFrontDemo3"]
+    var backImages = ["flashcardBackDemo","flashcardBackDemo2","flashcardBackDemo3"]
     
 //  flipping animation on flashcard tapped
     @IBAction func frontTapped(_ sender: Any){
         if isFlipped{
             isFlipped = false
-            let img = UIImage(named: "flashcardFrontDemo")
+            let img = UIImage(named: frontImages[currentFlashcard])
             cardIMG.setImage(img, for: .normal)
             UIView.transition(with: cardIMG, duration: 0.3, options: UIView.AnimationOptions.transitionFlipFromLeft, animations: nil, completion: nil)
         } else{
             isFlipped = true
-            let img = UIImage(named: "flashcardBackDemo")
+            let img = UIImage(named: backImages[currentFlashcard])
             cardIMG.setImage(img, for: .normal)
             UIView.transition(with: cardIMG, duration: 0.3, options: UIView.AnimationOptions.transitionFlipFromRight, animations: nil, completion: nil)
         }
     }
+    
+    
+    @IBAction func nextFlashcardTapped(_ sender: Any){
+        
+//      when next button is tapped, sets the current flashcard to + 1
+        currentFlashcard = currentFlashcard + 1
+        
+//      sets flashcard front face to the next flashcard
+        let img = UIImage(named: frontImages[currentFlashcard])
+        cardIMG.setImage(img, for: .normal)
+        
+//      increases progress bar by 10%
+        self.currentProgress.completedUnitCount += 1
+        let progressFloat = Float(self.currentProgress.fractionCompleted)
+        self.progressView.setProgress(progressFloat, animated: true)
+        
+    }
+    
+    
+    
 }
