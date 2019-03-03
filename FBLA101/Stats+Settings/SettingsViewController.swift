0dd6e4954e36f67cb80646b762467a59a4485c33
Gonzalo Birrueta <silentcr7@gmail.com>
2019-03-03 03:46:53 -0800

    commented settings class

--- FBLA101/Stats+Settings/SettingsViewController.swift
+++ FBLA101/Stats+Settings/SettingsViewController.swift
@@ -14,22 +14,19 @@ import Firebase
 
 class SettingsViewController: UITableViewController, MFMailComposeViewControllerDelegate{
     
-    
+    //  Outlets for all dynamic UI Elements
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var LogoutLabel: UILabel!
-    
     @IBOutlet weak var SignInLabel: UILabel!
-
     
-    
+    //sets the email and username cells to the current user logged in, if its a guest user a sign in button is displayed, and if its a user, a log out button is displayed.
     override func viewDidLoad() {
         super.viewDidLoad()
         if let user = Auth.auth().currentUser{
             emailLabel.text = Auth.auth().currentUser?.email
             usernameLabel.text = Auth.auth().currentUser?.displayName
             guestUser = false
-            
         }
         if guestUser == true{
             SignInLabel.isHidden = false
@@ -40,6 +37,7 @@ class SettingsViewController: UITableViewController, MFMailComposeViewController
         }
     }
     
+    //creates static table view that contains the settings, also controls log out/sign in button. If report bug is pressed it brings up a contact form (email) to our support team.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 2 && indexPath.row == 0 {
             let mailComposeViewController = configureMailController()
